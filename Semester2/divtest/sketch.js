29a2b7738c6d8cf2f479be1fb56448dc89fc2012
Wesley Dowling <wezzhad@gmail.com>
2019-10-28 14:14:19 +1100

    update

--- Semester2/divtest/sketch.js
+++ Semester2/divtest/sketch.js
@@ -26,8 +26,7 @@ text_dict.class("didatic");
 text_dict.position(20, 20);
 text_dict.size(width- 20, height - 100);
 
-var $scrollableElement = document.querySelector('.didatic');
-scrollLock.enablePageScroll($scrollableElement);
+
 }
 
 
