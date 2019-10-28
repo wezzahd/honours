4a8e408182d91b269ca083194a2971d85bf34317
Wesley Dowling <wezzhad@gmail.com>
2019-10-28 20:26:50 +1100

    update

--- Semester2/week14divtest2/sketch.js
+++ Semester2/week14divtest2/sketch.js
@@ -244,7 +244,7 @@ function mousePressed() {
   }
 
 
-  if (mouseY > height -100 && mouseY < height){
+  if (mouseX > (width-90) && mouseX < (width) && mouseY > 0 && mouseY < 90) {
     instruction_toggle = !instruction_toggle;
     if (instruction_toggle == true){
       instructions();
