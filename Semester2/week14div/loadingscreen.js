c56f2094fa866e09b3cda2c2edecbef2494a732c
Wesley Dowling <wezzhad@gmail.com>
2019-10-28 16:08:59 +1100

    update

--- Semester2/week14div/loadingscreen.js
+++ Semester2/week14div/loadingscreen.js
@@ -45,8 +45,8 @@ text_dict.position(20,100);
 text_dict.size(width- 20, height - 100);
 
 var $scrollableElement = document.querySelector('.didatic');
-//scrollLock.disablePageScroll($scrollableElement);
-scrollLock.enablePageScroll();
+scrollLock.disablePageScroll($scrollableElement);
+//scrollLock.PageScroll();
 //
 }
 
